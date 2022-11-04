2a256b4c59b96803481030228d9a73dbfb55e1f4
Varsha <varsha01@nagarro.com>
2022-11-04 15:37:02 +0530

    Starter Code For DevOps Workshop

---
+++ src/main/java/com/devopssample/controller/FrontController.java
@@ -0,0 +1,20 @@
+package com.devopssample.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value = "/")
+public class FrontController{
+	
+	@RequestMapping(value = "", method = RequestMethod.GET)
+	public ModelAndView getHomeView(HttpServletRequest request) {
+		ModelAndView model = new ModelAndView("home");
+		return model;
+	}
+
+}
